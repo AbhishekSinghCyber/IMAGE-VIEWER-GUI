39c555f7bebf80d2f5dc243cebe815c57af7899b
AbhishekSinghCyber <abhishek15178@gmail.com>
2024-08-29 22:07:50 +0530

    Added Main Menubar

--- image_viewer.py
+++ image_viewer.py
@@ -3,7 +3,14 @@ from tkinter import filedialog
 import tkinter as tk
 from PIL import Image, ImageTk
 import os
+import tkinter.messagebox as tmsg
 
+
+root = Tk()
+root.title("Image Viewer Application")
+root.wm_attributes('-transparentcolor', 'red')
+root.config(bg='red')
+root.geometry("450x450")
 
 def show_image():
     global filename
@@ -16,13 +23,28 @@ def show_image():
         lbl.configure(image=img)
         lbl.image = img
 
+def appli():
+    tmsg.showinfo("About GUI", "This GUI Is Made In Python With The Help Of Tkinter And Pillow Library")
 
-root = Tk()
+def developer():
+    tmsg.showinfo("About Developer","Hello User,   I'm Abhishek Singh  the developer of this Image Viewer Application")
 
+# MainMenu And Dropdown Menu
+mainmenu = Menu(root)
+mainmenu.add_command(label="About GUI", command=appli)
+mainmenu.add_command(label="Developer", command=developer)
+mainmenu.add_command(label="Exit", command=quit)
+
+# Packing of menu
+root.config(menu=mainmenu)
+
+
+# Frame
 f = Frame(root, bg='red')
-f.pack(side=BOTTOM, padx=15, pady=15)
+f.pack(side=BOTTOM, padx=15, pady=8)
 
-lbl = Label(root)
+
+lbl = Label(root, text="Your Image Will Be Shown Here", width=35, bg="black", bd=180, fg="white", font=("lucida", 15))
 lbl.pack()
 
 button = Button(f, text="Select Image", width=15, bg='orange', bd=10, fg='blue', command=show_image)
@@ -30,8 +52,4 @@ button.pack(side=tk.LEFT)
 
 button2 = Button(f, text="Exit", width=15, bg='orange', bd=10, fg='blue', command=lambda: exit())
 button2.pack(side=tk.LEFT, padx=15)
-root.title("Image Viewer Application")
-root.wm_attributes('-transparentcolor', 'red')
-root.config(bg='red')
-root.geometry("450x450")
 root.mainloop()
